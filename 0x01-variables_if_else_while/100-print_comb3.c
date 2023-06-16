cd3b1707dda8afb464fc74586fb436860563d553
ayoub19993 <ayoub.chahirer@gmail.com>
2023-06-16 13:23:51 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible different
+ *              combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0, b;
+
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a + 48);
+				putchar(b + 48);
+				if (a + b != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
