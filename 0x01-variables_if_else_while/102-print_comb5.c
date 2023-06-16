93fe5d05a66985aaad05eef9b707b564ca8233cf
ayoub19993 <ayoub.chahirer@gmail.com>
2023-06-16 14:25:56 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 			if (b != a)
 			{
 				putchar((a / 10) + 48);
-				putchar(a % 10) + 48);
+				putchar((a % 10) + 48);
 				putchar(' ');
 				putchar((b / 10) + 48);
 				putchar((b % 10) + 48);
