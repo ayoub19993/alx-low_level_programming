2c9c3dd97e7026a9cb9063c99fd965b69ca987d2
ayoub19993 <ayoub.chahirer@gmail.com>
2023-06-16 14:23:19 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: write a program that prints all possible
+ * different combinations of two figits
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0, b;
+
+	while (a <= 99)
+	{
+		b = a;
+		while (b <= 99)
+		{
+			if (b != a)
+			{
+				putchar((a / 10) + 48);
+				putchar(a % 10) + 48);
+				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
